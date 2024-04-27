e95a24acc0ca0a85b8da0acd8d71b36de0eb8697
Dorfultariant <tunxero@hotmail.com>
2024-04-27 16:30:10 +0300

    Code verification

--- keypad_test/km/main.c
+++ keypad_test/km/main.c
@@ -1,3 +1,5 @@
+#include <cstdint>
+#include <math.h>
 #define F_CPU 16000000UL
 #define DATA_SIZE 16
 #define BAUD 9600
@@ -20,8 +22,11 @@ void KEYPAD_WaitForKeyPress();
 uint8_t KEYPAD_GetKey();
 */
 
-int main(void) {
+void read_keypad_code(char *dest, uint8_t code_len);
+int verify_code(char *to_be_checked, char *correct);
 
+int main(void) {
+  char correctCode[] = "0423";
   DDRB &= ~(1 << LED_BUILTIN);
 
   USART_Init(MYUBRR);
@@ -34,20 +39,39 @@ int main(void) {
   char c[5]; // [0, 1, 2, 3, 4]
 
   while (1) {
-    printf("Give numbers:\n");
+    read_keypad_code(c, 4);
 
-    for (uint8_t i = 0; i < 4; i++) {
-      c[i] = KEYPAD_GetKey();
-      printf("%c ", c[i]);
-    }
+    printf("\nYou inserted: %s\n", c);
+    c[4] = '\0';
+    uint8_t isValid = verify_code(c, correctCode);
 
-    printf("\nYou inserted: %a\n", c);
-
+    printf("Is code valid: %d\n", isValid);
     // Turn on the led
-    PORTB ^= (1 << LED_BUILTIN);
+    PORTB |= (1 << LED_BUILTIN);
 
     _delay_ms(500);
   }
 
   return 0;
+}
+
+void read_keypad_code(char *dest, uint8_t code_len) {
+  // DEBUG PRINT TO PUTTY
+  printf("Give %d. numbers:\n", code_len);
+
+  for (uint8_t i = 0; i < code_len; i++) {
+    dest[i] = KEYPAD_GetKey();
+    printf("%c ", dest[i]);
+  }
+}
+
+int verify_code(char *to_be_checked, char *correct) {
+  uint8_t i = 0;
+  while ((to_be_checked[i] != '\0') && (correct[i] != '\0')) {
+    if (to_be_checked[i] != correct[i]) {
+      return 0;
+    }
+    i++;
+  }
+  return 1;
 }
